86ae076494d88848da81ebbf330f4fdd29c0402c
Missbellum <enjaykanz@gmail.com>
2024-03-16 16:30:53 +0100

    Built Admin site
    Created models for site config

--- local_library/local_library/urls.py
+++ local_library/local_library/urls.py
@@ -25,8 +25,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
-urlpatterns = [
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+    
