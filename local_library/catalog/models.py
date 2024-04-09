27e99f2346379e0eb005826a7bc95e2e5b9dc034
Missbellum <enjaykanz@gmail.com>
2024-04-09 14:38:02 +0100

    Created unit tests

--- local_library/catalog/models.py
+++ local_library/catalog/models.py
@@ -81,17 +81,7 @@ class Book(models.Model):
         """Create a string for the Genre. This is required to display genre in Admin."""
         return ', '.join(genre.name for genre in self.genre.all()[:3])
 
-   
     display_genre.short_description = 'Genre'
-
-
-    def display_genre(self):
-        """Create a string for the Genre. This is required to display genre in Admin."""
-        return ', '.join(genre.name for genre in self.genre.all()[:3])
-
-   
-    display_genre.short_description = 'Genre'
-
 
     def __str__(self):
         """String for representing the Model object."""
